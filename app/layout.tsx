a66ba3550db3bc1631543c5927e41ac38357151b
akggautamasar <Worksbeyondworks@gmail.com>
2026-09-18 16:00:22 +0530

    Build AirPages reader core

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"AirPages",description:"Immersive page-by-page study reader"};
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="en"><body>{children}</body></html>}
